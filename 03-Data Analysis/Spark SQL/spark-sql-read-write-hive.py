ae0f5edd899534831a3b0da706e85f4e747b72ba
sebastian-ruiz <33075840+sebastian-ruiz@users.noreply.github.com>
2017-10-28 20:30:05 -0700

    Update and rename spark-sql-read-hive.py to spark-sql-read-write-hive.py

--- 03-Data Analysis/Spark SQL/spark-sql-read-write-hive.py
+++ 03-Data Analysis/Spark SQL/spark-sql-read-write-hive.py
@@ -12,10 +12,7 @@ rdd2 = rdd.map(lambda x: x.split(","))
 rdd3 = rdd2.map(lambda x: Row(name=(x[0]),age=int(x[1])))
 
 df = sqlContext.createDataFrame(rdd3)
-df.registerTempTable("people_temp")
 
-sqlContext.sql("DROP TABLE people")
-sqlContext.sql("CREATE TABLE IF NOT EXISTS people (name STRING, age INT)")
-sqlContext.sql("INSERT INTO people SELECT name, age FROM people_temp")
+df.write.saveAsTable("people",mode="overwrite")
 
-df2 = sqlContext.sql("SELECT * FROM people").show()
+sqlContext.sql("SELECT name,age FROM people").show()
