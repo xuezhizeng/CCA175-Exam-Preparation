0bc96e67873934318eb8c0c3afe0c6097e3bba67
sebastian-ruiz <33075840+sebastian-ruiz@users.noreply.github.com>
2017-10-28 20:01:46 -0700

    Update spark-sql-read-hive.py

--- 03-Data Analysis/Spark SQL/spark-sql-read-hive.py
+++ 03-Data Analysis/Spark SQL/spark-sql-read-hive.py
@@ -1,13 +1,21 @@
-# sc is an existing SparkContext.
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import HiveContext
+from pyspark.sql import HiveContext, Row
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 
 sqlContext = HiveContext(sc)
 
-sqlContext.sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING)")
-sqlContext.sql("LOAD DATA LOCAL INPATH '/user/cloudera/people.txt' INTO TABLE people")
+rdd = sc.textFile("/user/cloudera/people.txt")
 
-results = sqlContext.sql("FROM people SELECT key, value").collect()
+rdd2 = rdd.map(lambda x: x.split(","))
+rdd3 = rdd2.map(lambda x: Row(name=(x[0]),age=int(x[1])))
+
+df = sqlContext.createDataFrame(rdd3)
+df.registerTempTable("people_temp")
+
+sqlContext.sql("DROP TABLE people")
+sqlContext.sql("CREATE TABLE IF NOT EXISTS people (name STRING, age INT)")
+sqlContext.sql("INSERT INTO people SELECT name, age FROM people_temp")
+
+df2 = sqlContext.sql("SELECT * FROM people").show()
