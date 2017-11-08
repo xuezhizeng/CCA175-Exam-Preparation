2f0718e1e65a0aafdf827016cfe60a089249897c
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-07 16:21:57 -0800

    Update spark-sql-hive-create-table.py

--- 03-Data Analysis/Spark SQL/Metastore tables/spark-sql-hive-create-table.py
+++ 03-Data Analysis/Spark SQL/Metastore tables/spark-sql-hive-create-table.py
@@ -1,10 +1,16 @@
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import HiveContext
+from pyspark.sql import HiveContext, Row
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 sqlContext = HiveContext(sc)
 
 sqlContext.sql("CREATE TABLE IF NOT EXISTS people(name STRING, age INT)")
+
+rdd = sc.textFile("/user/cloudera/spark/people.txt").map(lambda x: x.split(",")).map(lambda x: Row( name=x[0], age=x[1] ))
+
+df = sqlContext.createDataFrame(rdd)
+
+df.show()
 
 
