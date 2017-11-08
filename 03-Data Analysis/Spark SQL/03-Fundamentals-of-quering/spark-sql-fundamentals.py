bbe8c0be2321eb591dcc807e814d8015ce10ca05
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-08 14:32:05 -0800

    Create spark-sql-fundamentals.py

---
+++ 03-Data Analysis/Spark SQL/03-Fundamentals-of-quering/spark-sql-fundamentals.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+
+sqlContext = SQLContext(sc)
+
+df = sqlContext.read.json("/user/cloudera/spark/people.json")
+
+df.show()
+
+df.select(df.name).show()
+
+df.select(df['name'],df['age']+1).show()
+
+df.select(df['age']>21).show()
+
+df.groupBy("age").count().show()
