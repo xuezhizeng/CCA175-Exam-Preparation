9155b671ae79f48f68667863eccb3b91a2a04027
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-09 08:10:40 -0800

    Create spark-sql-avro-to-json.py

---
+++ 02-Transform Stage and Store/Spark Core/Read-Write different file formats/spark-sql-avro-to-json.py
@@ -0,0 +1,11 @@
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+sqlContext = HiveContext(sc)
+
+df = sqlContext.read.format('com.databricks.spark.avro').load("/user/cloudera/spark/users.avro")
+
+df.write.json("/user/cloudera/spark/users.json")
