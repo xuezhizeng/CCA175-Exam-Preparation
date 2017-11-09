cd6f6a26f9c1964095534935cf79668da58be7f5
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-09 09:36:41 -0800

    Create accumulators.py

---
+++ 02-Transform Stage and Store/Spark Core/shared variables/accumulators.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+
+rdd = sc.textFile("/user/cloudera/spark/logs.txt").map(lambda x: x.split(" ")).map(lambda x: x[1]).filter(lambda x: x == "DEBUG")
+
+print rdd.collect()
+
+accum = sc.accumulator(0)
+
+rdd.foreach(lambda x: accum.add(1))
+
+print accum.value
