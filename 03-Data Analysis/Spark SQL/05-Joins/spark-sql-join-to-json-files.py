9a31a3a74900859a54b5eb26be4a2411a513817d
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-10 08:55:27 -0800

    Update spark-sql-join-to-json-files.py

--- 03-Data Analysis/Spark SQL/05-Joins/spark-sql-join-to-json-files.py
+++ 03-Data Analysis/Spark SQL/05-Joins/spark-sql-join-to-json-files.py
@@ -13,9 +13,8 @@ df.show()
 
 df2.show()
 
-# Calcule avg of salaries per department
 df.join(df2,df.id_dept==df2.id). \
         select(df2.name, df.salary). \
         groupBy(df2.name). \
-        avg(). \
+        avg('salary'). \
         show()
