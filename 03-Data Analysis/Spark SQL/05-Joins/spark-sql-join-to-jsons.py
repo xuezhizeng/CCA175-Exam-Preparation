2aa83fbfe91727068a8779eefe187282ce033107
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-10 08:15:56 -0800

    Create spark-sql-join-to-jsons.py

---
+++ 03-Data Analysis/Spark SQL/05-Joins/spark-sql-join-to-jsons.py
@@ -0,0 +1,20 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+
+df = sqlContext.read.json("/user/cloudera/spark/employees.json")
+
+df2 = sqlContext.read.json("/user/cloudera/spark/departments.json")
+
+df.show()
+
+df2.show()
+
+df.join(df2,df.id_dept==df2.id). \
+        select(df2.name, df.salary). \
+        groupBy(df2.name). \
+        avg(). \
+        show()
