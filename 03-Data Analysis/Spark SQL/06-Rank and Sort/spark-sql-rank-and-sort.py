7601f19eee2cdd9c8810d3a89f0c1de72054afd4
Sebastian Ruiz Martinez <33075840+sebastian-ruiz@users.noreply.github.com>
2017-11-11 01:57:49 -0800

    Create spark-sql-rank-and-sort.py

---
+++ 03-Data Analysis/Spark SQL/06-Rank and Sort/spark-sql-rank-and-sort.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext, Window
+
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+sqlContext = HiveContext(sc)
+
+df = sqlContext.read.json("/user/cloudera/spark/employees_to_rank.json")
+
+df.registerTempTable("employees")
+
+sqlContext.sql("SELECT name, RANK() OVER (ORDER BY salary) as rank FROM employees").show()
